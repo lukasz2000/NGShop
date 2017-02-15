8ce25b903b3be9d26122b969bd100f0e6d20bd2f
Lukasz Kwiecien <l.kwiecien@ernabo.pl>
2017-02-15 08:45:27 +0100

    clean up code

--- js/application.js
+++ js/application.js
@@ -1,82 +1,80 @@
-'use strict';
-
-var app = angular.module('app', ['ngRoute','angular-storage','controllersNavigation','controllersAdmin','controllersSite','myServices']);
-
-app.config( ['$routeProvider','$httpProvider',function($routeProvider,$httpProvider) {
+angular
+.module('app', ['ngRoute','angular-storage','ControllersNavigation','ControllersAdmin','ControllersSite','myServices'])
+.config( ['$routeProvider','$httpProvider', function($routeProvider, $httpProvider){
 
 
 	// ================== Admin Products ====================
 
-	$routeProvider.when('/admin/products',{
-		controller: 'products',
+	$routeProvider.when('/admin/products', {
+		controller: 'ProductsController',
 		templateUrl: 'partials/admin/products.html'
 	});
 
-	$routeProvider.when('/admin/product/edit/:id',{
-		controller: 'productEdit',
+	$routeProvider.when('/admin/product/edit/:id', {
+		controller: 'ProductEditController',
 		templateUrl: 'partials/admin/product-edit.html'
 	});
 
-	$routeProvider.when('/admin/product/create',{
-		controller: 'productCreate',
+	$routeProvider.when('/admin/product/create', {
+		controller: 'ProductCreateController',
 		templateUrl: 'partials/admin/product-create.html'
 	});
 
 	// ================== Admin Users ====================
 
-	$routeProvider.when('/admin/users',{
-		controller: 'users',
+	$routeProvider.when('/admin/users', {
+		controller: 'UsersController',
 		templateUrl: 'partials/admin/users.html'
 	});
 
-	$routeProvider.when('/admin/user/edit/:id',{
-		controller:'userEdit',
+	$routeProvider.when('/admin/user/edit/:id', {
+		controller: 'UserEditController',
 		templateUrl: 'partials/admin/user-edit.html'
 	});
 
-	$routeProvider.when('/admin/user/create',{
-		controller: 'userCreate',
+	$routeProvider.when('/admin/user/create', {
+		controller: 'UserCreateController',
 		templateUrl: 'partials/admin/user-create.html'
 	});
 
 	// ================== Admin Orders ====================
 
-	$routeProvider.when('/admin/orders',{
-		controller: 'orders',
+	$routeProvider.when('/admin/orders', {
+		controller: 'OrdersController',
 		templateUrl: 'partials/admin/orders.html'
 	});
 
 	// ================== Site Products ====================
 
-	$routeProvider.when('/products',{
-		controller: 'siteProducts',
+	$routeProvider.when('/products', {
+		controller: 'SiteProductsController',
 		templateUrl: 'partials/site/products.html'
 	});
 
-	$routeProvider.when('/product/:id',{
-		controller: 'siteProduct',
+	$routeProvider.when('/product/:id', {
+		controller: 'SiteProductController',
 		templateUrl: 'partials/site/product.html'
 	});
 
-	$routeProvider.when('/cart',{
-		controller: 'cartCtrl',
+	$routeProvider.when('/cart', {
+		controller: 'CartController',
 		templateUrl: 'partials/site/cart.html'
 	});
 
-	$routeProvider.when('/orders',{
-		controller: 'siteOrders',
-		templateUrl: 'partials/site/orders.html'
+	$routeProvider.when('/orders', {
+		controller : 'SiteOrdersController',
+		templateUrl : 'partials/site/orders.html'
 	});
 
 	// ================ Login & Register ==================
 
-	$routeProvider.when('/login',{
-		controller: 'login',
+	$routeProvider.when('/login', {
+		controller: 'LoginController',
 		templateUrl: 'partials/site/login.html'
 	});
 
-	$routeProvider.when('/register',{
-		controller: 'register',
+	$routeProvider.when('/register', {
+		controller: 'RegisterController',
 		templateUrl: 'partials/site/register.html'
 	});
 
@@ -85,7 +83,6 @@ app.config( ['$routeProvider','$httpProvider',function($routeProvider,$httpProvi
 	$routeProvider.otherwise({
 		redirectTo: '/products'
 	});
-
 }]);
 
 
